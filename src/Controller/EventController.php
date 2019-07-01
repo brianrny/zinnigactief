859333a1778c66c4c21b7e475bed2b3638cd1968
Batuhan Gungor <batuboy2015@hotmail.com>
2019-07-01 14:28:46 +0200

    .

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -6,22 +6,29 @@ use App\Entity\Event;
 use App\Form\EventType;
 use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
 
 /**
  * @Route("/event")
  */
-class EventController extends AbstractController
+class EventController extends Controller
 {
     /**
      * @Route("/", name="event_index", methods={"GET"})
      */
-    public function index(EventRepository $eventRepository): Response
+    public function index(EventRepository $eventRepository, Request $request): Response
     {
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+        $eventRepository->findAll(), /* query NOT result */
+            $request->query->getInt('page', 1), 6);
+
         return $this->render('event/index.html.twig', [
-            'events' => $eventRepository->findAll(),
+            'events' => $pagination,
         ]);
     }
 
