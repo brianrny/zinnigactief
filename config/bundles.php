6ab1629ea4bc2cb41495d1552eae40c50706eb6c
memooh <m.ovdb@outlook.com>
2019-07-03 13:59:09 +0200

    mehmet

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
